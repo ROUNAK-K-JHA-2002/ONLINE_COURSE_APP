32b40b513bcce007ff2715807139198b7c527f53
ROUNAK KUMAR JHA <rounakjha291202.rar@gmail.com>
2022-08-03 19:41:52 +0530

    Synced firebase storage with adding videis option

--- lib/Screens/AddVideos.dart
+++ lib/Screens/AddVideos.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -12,7 +13,8 @@ class AddVideos extends StatefulWidget {
 
 class _AddVideosState extends State<AddVideos> {
   final TextEditingController titleController = TextEditingController();
-  final _formkey = GlobalKey<FormState>();
+  final TextEditingController ytLinkController = TextEditingController();
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     final TitleInput = TextFormField(
@@ -22,6 +24,13 @@ class _AddVideosState extends State<AddVideos> {
       keyboardType: TextInputType.text,
       onSaved: (value) {
         titleController.text = value!;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please Enter a Video Title");
+        }
+
+        return null;
       },
       textInputAction: TextInputAction.next,
       decoration: InputDecoration(
@@ -33,20 +42,16 @@ class _AddVideosState extends State<AddVideos> {
     final YTLinkInput = TextFormField(
       autofocus: false,
       autocorrect: true,
-      controller: titleController,
+      controller: ytLinkController,
       keyboardType: TextInputType.text,
       onSaved: (value) {
         titleController.text = value!;
       },
       validator: (value) {
-        RegExp regex = RegExp(
-            "?:https?://)?(?:www.)?youtu(?:.be/|be.com/S*(?:watch|embed)(?:(?:(?=/[-a-zA-Z0-9_]{11,}(?!S))/)|(?:S*v=|v/)))([-a-zA-Z0-9_]{11,}");
         if (value!.isEmpty) {
           return ("Please Enter a Youtube Link");
         }
-        if (!regex.hasMatch(value)) {
-          return ("Please Enter a valid youtube link");
-        }
+
         return null;
       },
       textInputAction: TextInputAction.next,
@@ -65,30 +70,59 @@ class _AddVideosState extends State<AddVideos> {
         padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.02),
         minWidth: MediaQuery.of(context).size.width,
         child: const Text(
-          "Sign Up",
+          "Add Video",
           style: TextStyle(
               fontWeight: FontWeight.w600, fontSize: 20.0, color: Colors.white),
         ),
-        onPressed: () {},
+        onPressed: () {
+          SendCourseData(
+              title: titleController.text, link: ytLinkController.text);
+          getData();
+        },
       ),
     );
     print(widget.SubjectName);
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: Text("Add ${widget.SubjectName} Videos"),
       ),
       body: Container(
-        color: Colors.amber,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromARGB(235, 240, 240, 130),
+              Color.fromARGB(245, 252, 223, 237),
+              Color.fromARGB(245, 165, 190, 255),
+            ],
+          ),
+        ),
         padding: EdgeInsets.symmetric(
             vertical: MediaQuery.of(context).size.width * 0.1,
             horizontal: MediaQuery.of(context).size.width * 0.05),
         child: Container(
             padding: EdgeInsets.symmetric(
                 horizontal: MediaQuery.of(context).size.width * 0.05),
-            color: Colors.white,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(125, 255, 255, 255),
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: const [
+                BoxShadow(
+                  color: Color.fromARGB(200, 248, 232, 248),
+                  blurRadius: 5.0, // soften the shadow
+                  spreadRadius: 5.0, //extend the shadow
+                  offset: Offset(
+                    5.0, // Move to right 10  horizontally
+                    5.0, // Move to bottom 10 Vertically
+                  ),
+                )
+              ],
+            ),
             child: Form(
               key: _formkey,
-              child: Column(children: [
+              child: ListView(children: [
                 const SizedBox(
                   height: 50,
                 ),
@@ -103,5 +137,23 @@ class _AddVideosState extends State<AddVideos> {
             )),
       ),
     );
+  }
+
+  // ignore: non_constant_identifier_names
+  Future SendCourseData({required String title, required String link}) async {
+    final data = FirebaseFirestore.instance.collection("Courses_link").doc();
+    final json = {
+      "title": title,
+      "link": link,
+    };
+    if (_formkey.currentState!.validate()) await {await data.set(json)};
+  }
+
+  CollectionReference collectionReference =
+      FirebaseFirestore.instance.collection("Courses_link");
+  Future<void> getData() async {
+    QuerySnapshot querySnapshot = await collectionReference.get();
+    final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+    print(allData[1]);
   }
 }
